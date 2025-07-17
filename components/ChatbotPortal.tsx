a4f7d9e691747407f689351289b37c35f7b22c13
ashishmantoit <ashish@mangoitsolutions.com>
2025-07-17 19:04:40 +0530

    Delete Unwanted Code

--- components/ChatbotPortal.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-
-// Dynamically import to ensure nothing breaks during SSR
-const InvoiceChatbot = dynamic(() => import("./InvoiceChatbot"), { ssr: false });
-
-export default function ChatbotPortal() {
-  return <InvoiceChatbot />;
-}
--- migrations/20250613_add_client_company_name_to_invoices.sql
+++
@@ -1,2 +0,0 @@
--- Migration: Add client_company_name to invoices table
-ALTER TABLE invoices ADD COLUMN client_company_name VARCHAR(255) AFTER client_name;
