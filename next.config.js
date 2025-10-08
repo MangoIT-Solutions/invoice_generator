0a87e970aefe763cab6ca2544888116698005ed7
harshmangoit <harsh.raikwar@mangotisolutions.in>
2025-10-08 11:45:26 +0530

    Update next.config.js also use BASE_URL from env

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, // chatbot's origin
+          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_BASE_URL }, // chatbot's origin
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
@@ -24,5 +24,5 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
 
